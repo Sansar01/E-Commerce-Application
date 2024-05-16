5fc4c02889679ac71fc41f0ae6877777c4a8ae44
SansarTiwari <st9889477@gmail.com git config --global user.name SansarTiwari>
2024-05-16 18:29:25 +0530

    first commit

---
+++ E-commerce/src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../Header/Header.css'
+
+function Header() {
+  return (
+    <header>
+        <div className="container-fluid">
+            
+        </div>
+    </header>
+  )
+}
+
+export default Header
