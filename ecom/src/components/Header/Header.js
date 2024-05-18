66f4383b287fc8cda532ab7bdca3b550308f15d6
SansarTiwari <st9889477@gmail.com git config --global user.name SansarTiwari>
2024-05-18 17:11:38 +0530

    Header start implementing

--- ecom/src/components/Header/Header.js
+++ ecom/src/components/Header/Header.js
@@ -1,19 +1,35 @@
-import React from 'react'
-import Logo from '../../assets/images/logo.svg'
-import '../Header/Header.css'
+import React from "react";
+import Logo from "../../assets/images/logo.svg";
+import "../Header/Header.css";
 
 function Header() {
   return (
-        <header>
-               <div className='container-fluid'>
-                <div className="row">
-                    <div className="col-sm-2">
-                        <img src={Logo}/>
-                    </div>
-                </div>
-               </div>
-        </header>
-  )
+    <>
+      <header>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-2">
+            <img src={Logo} />
+          </div>
+           {/* {header search start here} */}
+
+        <div className="col-sm-5">
+          <div className="headerSearch d-flex align-items-center">
+            <div className="selectDrop cursor">
+              All Categories
+            </div>
+            <div className="search">
+              <input type="text" placeholder="Search for items..."/>
+            </div>
+          </div>
+        </div>
+
+        {/* {header search end here} */}
+        </div>
+      </div>
+    </header>
+    </>
+  );
 }
 
-export default Header
+export default Header;
