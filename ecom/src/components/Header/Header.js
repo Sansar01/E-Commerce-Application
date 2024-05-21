9695e1fa4b52d74d33c95625e0351e576dc22cb9
SansarTiwari <st9889477@gmail.com git config --global user.name SansarTiwari>
2024-05-21 17:42:22 +0530

    Added axios dependency and implemented country list API call in Header component; updated Select component to accept props and display selected item; modified CSS files for Header and Select components.

--- ecom/src/components/Header/Header.js
+++ ecom/src/components/Header/Header.js
@@ -1,10 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Logo from "../../assets/images/logo.svg";
 import "../Header/Header.css";
 import SearchIcon from '@mui/icons-material/Search';
 import Select from '../selectDrop/Select'
+import axios from 'axios'
 
 function Header() {
+
+     const[Categories,setCategories] = useState([
+      'All Categories',
+      'Milks and Dairies',
+      'Wines & Drinks',
+      'Clothing & beauty',
+      'Fresh Seafood',
+      'Pet Foods & Toy',
+      ' Fast food',
+      'Baking Material',
+      'Vegetables',
+      'Noodles & Rice',
+      'Ice cream'
+     ])
+
+     const countryList = [];
+
+      useEffect(()=>{
+        getCountry('https://countriesnow.space/api/v0.1/countries/')
+      },[])
+
+
+      const getCountry = async (url)=>{
+        try {
+          await axios.get(url).then((res)=>{
+              if(res!=null)
+              {
+                   res.data.data.map((item,index)=>(countryList.push(item.country)))
+                   
+              }
+          })
+        } catch (error) {
+          console.log(error.message);
+        }
+      }
+
   return (
     <>
       <header>
@@ -14,18 +51,21 @@ function Header() {
             <img src={Logo} />
           </div>
            {/* {header search start here} */}
-
         <div className="col-sm-5">
           <div className="headerSearch d-flex align-items-center">
-          <Select/>
+          <Select data={Categories} placeholder={"All Categories"}/>
             <div className="search">
               <input type="text" placeholder="Search for items..."/>
              <SearchIcon className="searchIcon cursor"/>
             </div>
           </div>
         </div>
-
         {/* {header search end here} */}
+        <div className="col-sm-5">
+         <div className="countryWrapper">
+         <Select data={countryList} placeholder={"Your Location"}/>
+         </div>
+        </div>
         </div>
       </div>
     </header>
--- ecom/src/components/selectDrop/Select.js
+++ ecom/src/components/selectDrop/Select.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import "../selectDrop/Select.css";
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { ClickAwayListener } from "@mui/base/ClickAwayListener";
 
-function Select() {
+function Select(props) {
   const [isOpenSelect, setisOpenSelect] = useState(false);
   const [selectedIndex, setSelectedIndex] = useState(0);
+  const [selectedItem, setselectedItem] = useState(props.placeholder);
+
+  console.log(props.placeholder);
 
   const openSelect = () => {
     setisOpenSelect(!isOpenSelect);
@@ -12,36 +16,120 @@ function Select() {
 
   const closeSelect = (Index) => {
     setSelectedIndex(Index);
-    setisOpenSelect(!isOpenSelect)
+
+    //Access ul elements
+    var li = document.querySelector(".searchResults");
+
+    //After collecting  all collection of li we getting value of children with index
+    var item = li.children[Index];
+
+    setisOpenSelect(!isOpenSelect);
+
+    // set Selected Item
+    setselectedItem(item.textContent);
+
+    //changing the value of css class
+    if (item.textContent.length >= 17) {
+      var css = document.styleSheets[3].cssRules[2].style;
+      css.setProperty("width", "30%");
+    } else {
+      var css = document.styleSheets[3].cssRules[2].style;
+      css.setProperty("width", "27%");
+    }
   };
-    
 
   return (
-    <div className="selectDropWrapper cursor position-relative">
-      <span className="openSelect" onClick={openSelect}>
-        All Categories <KeyboardArrowDownIcon className="arrow"/>
-      </span>
-      {isOpenSelect && (
-        <div className="selectDrop">
-          <div className="searchField">
-            <input type="text" />
+    <ClickAwayListener onClickAway={()=>setisOpenSelect(false)}>
+      <div className="selectDropWrapper cursor position-relative">
+        <span className="openSelect" onClick={openSelect}>
+          {selectedItem}
+          <KeyboardArrowDownIcon className="arrow" />
+        </span>
+        {isOpenSelect && (
+          <div className="selectDrop">
+            <div className="searchField">
+              <input type="text" placeholder="Search here"/>
+            </div>
+            <ul className="searchResults">
+            <li key={0} onClick={()=>closeSelect(0)} className={`${selectedIndex === 0 ? "active" : ""}`}>{props.placeholder}</li>
+            {
+              props && props.data.map((category,Index)=>{
+              return(
+                <li key={Index+1} onClick={()=>closeSelect(Index+1)} className={`${selectedIndex === Index+1 ? "active" : ""}`}>{category}</li>
+              )
+            })
+            }
+              {/* <li
+                onClick={() => closeSelect(0)}
+                className={`${selectedIndex === 0 ? "active" : ""}`}
+              >
+                All Categories
+              </li>
+              <li
+                onClick={() => closeSelect(1)}
+                className={`${selectedIndex === 1 ? "active" : ""}`}
+              >
+                Milks and Dairies
+              </li>
+              <li
+                onClick={() => closeSelect(2)}
+                className={`${selectedIndex === 2 ? "active" : ""}`}
+              >
+                Wines & Drinks
+              </li>
+              <li
+                onClick={() => closeSelect(3)}
+                className={`${selectedIndex === 3 ? "active" : ""}`}
+              >
+                Clothing & beauty
+              </li>
+              <li
+                onClick={() => closeSelect(4)}
+                className={`${selectedIndex === 4 ? "active" : ""}`}
+              >
+                Fresh Seafood
+              </li>
+              <li
+                onClick={() => closeSelect(5)}
+                className={`${selectedIndex === 5 ? "active" : ""}`}
+              >
+                Pet Foods & Toy
+              </li>
+              <li
+                onClick={() => closeSelect(6)}
+                className={`${selectedIndex === 6 ? "active" : ""}`}
+              >
+                Fast food
+              </li>
+              <li
+                onClick={() => closeSelect(7)}
+                className={`${selectedIndex === 7 ? "active" : ""}`}
+              >
+                Baking material
+              </li>
+              <li
+                onClick={() => closeSelect(8)}
+                className={`${selectedIndex === 8 ? "active" : ""}`}
+              >
+                Vegetables
+              </li>
+              <li
+                onClick={() => closeSelect(9)}
+                className={`${selectedIndex === 9 ? "active" : ""}`}
+              >
+                Noodles & Rice
+              </li>
+              <li
+                onClick={() => closeSelect(10)}
+                className={`${selectedIndex === 10 ? "active" : ""}`}
+              >
+                Ice cream
+              </li> */}
+            </ul>
           </div>
-          <ul className="searchResults">
-                    <li onClick={()=>closeSelect(0)} className={`${selectedIndex === 0 ? 'active' :''}`}>All Categories</li>
-                    <li onClick={()=>closeSelect(1)} className={`${selectedIndex === 1 ? 'active' :''}`}>Milks and Dairies</li>
-                    <li onClick={()=>closeSelect(2)} className={`${selectedIndex === 2 ? 'active' :''}`}>Wines & Drinks</li>
-                    <li onClick={()=>closeSelect(3)} className={`${selectedIndex === 3 ? 'active' :''}`}>Clothing & beauty</li>
-                    <li onClick={()=>closeSelect(4)} className={`${selectedIndex === 4 ? 'active' :''}`}>Fresh Seafood</li>
-                    <li onClick={()=>closeSelect(5)} className={`${selectedIndex === 5 ? 'active' :''}`}>Pet Foods & Toy</li>
-                    <li onClick={()=>closeSelect(6)} className={`${selectedIndex === 6 ? 'active' :''}`}>Fast food</li>
-                    <li onClick={()=>closeSelect(7)} className={`${selectedIndex === 7 ? 'active' :''}`}>Baking material</li>
-                    <li onClick={()=>closeSelect(8)} className={`${selectedIndex === 8 ? 'active' :''}`}>Vegetables</li>
-                    <li onClick={()=>closeSelect(9)} className={`${selectedIndex === 9 ? 'active' :''}`}>Noodles & Rice</li>
-                    <li onClick={()=>closeSelect(10)} className={`${selectedIndex === 10 ? 'active' :''}`}>Ice cream</li>
-                </ul>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </ClickAwayListener>
   );
 }
 
