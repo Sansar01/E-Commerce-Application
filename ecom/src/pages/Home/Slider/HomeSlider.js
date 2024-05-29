2b39993e5e727fa55b0b9d13ca6a4e05587a6c55
SansarTiwari <st9889477@gmail.com git config --global user.name SansarTiwari>
2024-05-29 18:06:12 +0530

    "Added Font Awesome CSS, updated HomeSlider component with new slider images, styles, and newsletter banner"

--- ecom/src/pages/Home/Slider/HomeSlider.js
+++ ecom/src/pages/Home/Slider/HomeSlider.js
@@ -1,18 +1,19 @@
 import React from "react";
 import Slider from "react-slick";
 import "../Slider/Slider.css";
-import slider1 from '../../../assets/images/slider-1.png'
-import slider2 from '../../../assets/images/slider-2.png'
+import slider1 from "../../../assets/images/slider-1.png";
+import slider2 from "../../../assets/images/slider-2.png";
+
 
 function HomeSlider() {
   var settings = {
-    dots: false,
+    dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    fade:true,
-    arrows:true
+    fade: true,
+    arrows: true,
   };
 
   console.log("css ", document.styleSheets);
@@ -22,12 +23,31 @@ function HomeSlider() {
       <section className="container-fluid">
         <Slider {...settings} className="home_slider_Main">
           <div className="item">
-            <img src={slider1}/>
+            <img src={slider1} className="w-100"/>
+            <div className="info">
+              <h2 class="mb-4">
+                Don’t miss amazing
+                <br />
+                grocery deals
+              </h2>
+              <p>Sign up for the daily newsletter</p>
+            </div>
           </div>
           <div className="item">
-            <img src={slider2}/>
+            <img src={slider2} className="w-100"/>
+            <div className="info">
+              <h2 class="mb-3">
+                Fresh Vegetables
+                <br />
+                Big discount
+              </h2>
+              <p>Sign up for the daily newsletter</p>
+            </div>
           </div>
         </Slider>
+        <div className="newsletterBanner">
+           <input type="text" placeholder="Your Email Address"/>
+        </div>
       </section>
     </section>
   );
