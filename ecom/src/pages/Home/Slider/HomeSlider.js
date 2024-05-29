12e4aad32bf4435202ff023f8fdeaa821bd6adf3
SansarTiwari <st9889477@gmail.com git config --global user.name SansarTiwari>
2024-05-29 15:15:02 +0530

    "Updated HomeSlider.js and Slider.css: changed slider settings, added images, and modified CSS styles"

--- ecom/src/pages/Home/Slider/HomeSlider.js
+++ ecom/src/pages/Home/Slider/HomeSlider.js
@@ -1,40 +1,31 @@
 import React from "react";
 import Slider from "react-slick";
 import "../Slider/Slider.css";
-import "slick-carousel/slick/slick-theme.css";
+import slider1 from '../../../assets/images/slider-1.png'
+import slider2 from '../../../assets/images/slider-2.png'
 
 function HomeSlider() {
   var settings = {
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    fade:true,
+    arrows:true
   };
 
-  console.log("css ",document.styleSheets);
+  console.log("css ", document.styleSheets);
 
   return (
     <section className="homeSlider">
       <section className="container-fluid">
-        <Slider {...settings}>
-          <div>
-            <h3>1</h3>
+        <Slider {...settings} className="home_slider_Main">
+          <div className="item">
+            <img src={slider1}/>
           </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
+          <div className="item">
+            <img src={slider2}/>
           </div>
         </Slider>
       </section>
