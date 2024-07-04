bd465e416402d5db64ef5903c2dbaf7773b5d231
Sansar <you@example.com>
2024-07-04 22:03:13 +0530

    "Added react-inner-image-zoom and slick-carousel dependencies, updated package.json and package-lock.json, modified Details page CSS and JSX, and updated Slider CSS"

--- ecom/src/pages/Details/Details.jsx
+++ ecom/src/pages/Details/Details.jsx
@@ -2,10 +2,23 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import '../Details/Details.css'
 import Rating from '@mui/material/Rating'
+import InnerImageZoom from 'react-inner-image-zoom'
+import 'react-inner-image-zoom/lib/InnerImageZoom/styles.css'
+import Slider from 'react-slick'
 
 function Details() {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    arrows: true,
+  }
+
   return (
-    <section className="detailPage">
+    <section className="detailsPage">
       <div className="breadcrumbWrapper mb-4">
         <div className="container-fluid">
           <ul className="breadcrumb breadcrumb2 mb-0">
@@ -23,42 +36,47 @@ function Details() {
         <div className="row pt-3 pb-3">
           <div className="col-md-9 leftpart">
             <div className="row">
-              <div className="col-md-5 productZoom">
-
-              </div>
-              <div className="col-md-7 productInfo">
-                {/* productZoom code start here */}
-                <div className="col-md-3 productZoom"></div>
-                {/* productZoom code end here */}
-
-                {/* product info code start here */}
-                <div className="col-md-9 productInfo">
-                  <h3>Seeds of Change Organic Quinoa,Bown</h3>
-                  <div className="d-flex align-items-center">
-                    <Rating
-                      name="half-rating-read"
-                      defaultValue={2.5}
-                      precision={0.5}
-                      readOnly
-                    />
-                    <span className="text-light">(32 reviews)</span>
-                  </div>
-                  <div className="priceSec d-flex align-items-center mb-3">
-                    <span className="text-g">$38</span>
-                    <div className="ms-2 d-flex flex-column">
-                      <span className="text-g">26% Off</span>
-                      <span className="text-light">$52</span>
-                    </div>
-                  </div>
-                  <p>
-                    sfdsjfdshfaskfid jdfasdalfadklal f dshfahlfhd jshfl o o d dk
-                    jf{' '}
-                  </p>
+              {/* productzoom code start here */}
+              <div className="col-md-5">
+                <div className="productZoom">
+                  <InnerImageZoom
+                    zoomType="hover"
+                    zoomScale={2}
+                    src="https://h5p.org/sites/default/files/h5p/content/1209180/images/file-6113d5f8845dc.jpeg"
+                  />
                 </div>
-                {/*  */}
+                <Slider {...settings} className="home_slider_Main"></Slider>
               </div>
+              {/* productzoom code end here */}
+
+              {/* productInfo code start here */}
+              <div className="col-md-7 productInfo">
+                <h1>Seeds of Change Organic Quinoa,Bown</h1>
+                <div className="d-flex align-items-center mb-4 mt-3">
+                  <Rating
+                    name="half-rating-read"
+                    defaultValue={2.5}
+                    precision={0.5}
+                    readOnly
+                  />
+                  <span className="text-light ms-2 ">(32 reviews)</span>
+                </div>
+                <div className="priceSec d-flex align-items-center mb-3">
+                  <span className="text-g priceLarge">$38</span>
+                  <div className="ms-3 d-flex flex-column">
+                    <span className="text-org">26% Off</span>
+                    <span className="text-light">$52</span>
+                  </div>
+                </div>
+                <p>
+                  sfdsjfdshfaskfid jdfasdalfadklal f dshfahlfhd jshfl o o d dk
+                  jf{' '}
+                </p>
+              </div>
+              {/* productInfo code start here */}
             </div>
           </div>
+          <div className="col-md-3 part2"></div>
         </div>
       </div>
     </section>
--- ecom/src/pages/Home/Slider/HomeSlider.js
+++ ecom/src/pages/Home/Slider/HomeSlider.js
@@ -3,11 +3,10 @@ import Slider from 'react-slick'
 import '../Slider/Slider.css'
 import slider1 from '../../../assets/images/slider-1.png'
 import slider2 from '../../../assets/images/slider-2.png'
-import Button from '@mui/material/Button'
-import SendOutlinedIcon from '@mui/icons-material/SendOutlined'
 import NewLetter from '../../../components/Newsletter/NewsLetter'
 
 function HomeSlider() {
+  
   var settings = {
     dots: true,
     infinite: true,
